b6bb4000340dfa3bad94c21cc8441c9b2523181c
Matheus Melo <math.li.melo@gmail.com>
2020-08-13 22:39:34 -0300

    Correções Necessárias

--- index.js
+++ index.js
@@ -11,8 +11,20 @@ const wsServer = new webSocketsServer({
 });
 
 const clients = {};
+const userQueue = [
+  {
+    id: "Vazio",
+    name: "Vazio",
+  },
+];
 
-let busy = false;
+function getNextVoter() {
+  if (userQueue.length > 1) userQueue = userQueue.slice(1);
+}
+
+function addUserToQueue(user) {
+  userQueue.push(user);
+}
 
 function objIsEmpty(obj) {
   if (Object.keys(obj).length === 0 && obj.constructor === Object) {
@@ -43,41 +55,72 @@ const getUniqueID = () => {
 
 wsServer.on("request", function (request) {
   let userId = getUniqueID();
-  console.log(
-    new Date() +
-      " Recieved a new connection from origin " +
-      request.origin +
-      "."
-  );
+  // console.log(
+  //   new Date() +
+  //     " Recieved a new connection from origin " +
+  //     request.origin +
+  //     "."
+  // );
+  console.log("ALGUEM CONECTOU");
 
   //Aceitar a conexao
   const connection = request.accept(null, request.origin);
   clients[userId] = connection;
-  console.log(
-    "connected: " + userId + " in " + Object.getOwnPropertyNames(clients)
-  );
+  // console.log(
+  //   "connected: " + userId + " in " + Object.getOwnPropertyNames(clients)
+  // );
 
-  connection.on("message", function (message) {
-    if (message.utf8Data === "stop-conference") {
-      clients = removeUser(clients, userId);
-      busy = false;
-      console.log("\n***Someone stopped a conference***\n");
-    } else if (objIsEmpty(clients)) {
-      busy = false;
-    }
-    //Controle de fila de usuários
-    for (key in clients) {
-      if (!busy) {
-        busy = true;
-        clients[key].sendUTF("start-conference");
-        console.log(
-          "\n***This client started a conference: " + clients[key] + "***\n"
-        );
-      }
+  connection.on("message", function (msg) {
+    message = JSON.parse(msg.data);
+    switch (message.type) {
+      case "enter-queue":
+        addUserToQueue(message.data);
+        console.log(`\n\n***Tentando Entrar ${message.data}***\n\n`);
+        break;
+      case "move-queue":
+        getNextVoter();
+        for (key in clients) {
+          clients[key].send(
+            JSON.stringify({
+              type: "new-current-user",
+              id: userQueue[0].id,
+              name: userQueue[0].name,
+            })
+          );
+        }
+        break;
+      case "get-head-of-queue":
+        for (key in clients) {
+          clients[key].send(
+            JSON.stringify({
+              type: "new-current-user",
+              id: userQueue[0].id,
+              name: userQueue[0].name,
+            })
+          );
+        }
+        break;
+      case "get-lenght-of-queue":
+        for (key in clients) {
+          clients[key].send(
+            JSON.stringify({
+              type: "lenght-of-queue",
+              value: userQueue.lenght,
+            })
+          );
+        }
+        break;
+      case "set-sessionId":
+        for (key in clients) {
+          clients[key].send(
+            JSON.stringify({
+              type: "your-sessionId",
+              value: message.data,
+            })
+          );
+        }
+        break;
+      default:
     }
   });
 });
-
-// Cliente manda uma mensagem, uma string "connect-me"
-// A partir dai ele esta na fila para receber a mensagem "start-conference"
-// Quando sair da conferencia o cliente deve mandar "stop-conference"
